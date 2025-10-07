51b204b7a2fc7f45cddc6a77e0021c3ed0d30d08
Juan's codes <curielgarciajuanjesus@gmail.com>
2025-10-07 10:51:07 -0600

    Fragmentos clase de kikin

---
+++ Fragmentos/app/src/main/java/com/example/fragmentos/rojo.java
@@ -0,0 +1,32 @@
+package com.example.fragmentos;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class rojo extends Fragment implements View.OnClickListener {
+
+
+    Button botoncito;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View vistita;
+        vistita = inflater.inflate(R.layout.fragment_rojo, container, false);
+
+        botoncito = vistita.findViewById(R.id.boton);
+        botoncito.setOnClickListener(this);
+        return vistita;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(getContext(), "Hola kikin", Toast.LENGTH_SHORT).show();
+    }
+}
